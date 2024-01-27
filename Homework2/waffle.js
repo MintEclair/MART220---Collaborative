f9f30e6161518e410d25e1d39f34efc4099a8c3b
Kenny-c-W <143756920+Kenny-c-W@users.noreply.github.com>
2024-01-26 17:48:23 -0700

    fav food
    chat fav and my fav

---
+++ Homework2/waffle.js
@@ -0,0 +1,17 @@
+function setup()
+{
+    createCanvas(400, 400);
+}
+
+function draw()
+{
+    background(300);
+     //waffle
+     circle (200,200,100);
+     line (200, 250, 200, 150);
+     line (150, 200, 250, 200);
+     line (155, 175, 245, 175);
+     line (155, 225, 245, 225);
+     line (175, 155, 175, 245);
+     line (225, 155, 225, 245);
+}
